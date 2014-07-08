1823b2a6cf764729aa36c05b834128d4a6c244d5
Simon Milz <sm@milzer.de>
2014-07-08 20:42:05 +0200

    Generalized parent id for creating a new folder

--- js/ProjectEditDialog.js
+++ js/ProjectEditDialog.js
@@ -91,8 +91,8 @@ Tine.FundProject.ProjectEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog, 
         });
 		this.documentsPanel =  new Tine.Document.DocumentsTabPanel({
             app: Tine.Tinebase.appMgr.get('FundProject'),
-            record_id: (this.record) ? this.record.id : '',
             record: this.record,
+            parent_id: Tine.Document.registry.get('config').fundproject_id,
             record_model: this.appName + '_Model_' + this.recordClass.getMeta('modelName')
         });
 		var fields = Tine.FundProject.ProjectFormFields.get();
